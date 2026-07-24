05e91962d38437ca6910a615dd28b50b912cf451
Sezylrin <alastairlu2002@gmail.com>
2026-07-25 07:24:03 +1000

    Railgun polished

--- Assets/Scripts/Player/Weapon/RailgunBehaviour.cs
+++ Assets/Scripts/Player/Weapon/RailgunBehaviour.cs
@@ -8,6 +8,8 @@ public class RailgunBehaviour : RangedWeapon
     [SerializeField]
     private float chargeUpDur;
     private PlayerController playerController;
+    [SerializeField]
+    private ParticleSystem charging;
     protected override void Start()
     {
         base.Start();
@@ -20,10 +22,18 @@ public class RailgunBehaviour : RangedWeapon
     public override void Attack(Vector3 attackDir)
     {
         startupTimer.RestartTimer();
+        charging.Play(true);
+        anim.Play("ChargeUp");
     }
 
     private void fireWeapon(object sender, EventArgs e)
     {
-        ShootBullet(playerController.GetAttackDir());
+        anim.Play("Attack");
+        charging.Stop();
+        Debug.Log(playerController.GetAttackDir());
+        Vector3 dir = playerController.GetAttackDir();
+        dir.y = 0;
+        dir.Normalize();
+        ShootBullet(dir);
     }
 }
