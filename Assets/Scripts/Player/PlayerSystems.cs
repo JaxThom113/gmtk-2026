daa46fa3501e68525145e722ad23b913e285b733
Sezylrin <alastairlu2002@gmail.com>
2026-07-26 06:14:34 +1000

    Iframe visuals

--- Assets/Scripts/Player/PlayerSystems.cs
+++ Assets/Scripts/Player/PlayerSystems.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerSystems : MonoBehaviour
 {
+    [Header("Core")]
+    [SerializeField]
+    private PlayerComponentManager PCM;
     [Header("SO References")]
     [SerializeField]
     private IntSO playerCurrentHealthSO;
@@ -19,12 +22,14 @@ public class PlayerSystems : MonoBehaviour
     private int healthDrainRate; // how many seconds are subtracted per second
     [SerializeField]
     private float secondLength; // length of a second, decrease to make seconds go by faster
-
-    [Header("Other")]
-    [SerializeField]
-    private PlayerComponentManager PCM;
+    [Header("Iframe")]
     [SerializeField]
     private float iframeDur;
+    [SerializeField]
+    private Transform shield;
+    private Vector3 scaleSize;
+    [SerializeField]
+    private float shieldTweenDur;
 
     [Header("death")]
     [SerializeField]
@@ -34,12 +39,17 @@ public class PlayerSystems : MonoBehaviour
     [ColorUsage(true, true),SerializeField]
     private Color deathColor;
 
+
+
     private int timerPos = (int)PlayerTimer.healthDrain;
     private int iFrames = (int)PlayerTimer.Iframes;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        scaleSize = shield.localScale;
+        shield.localScale = Vector3.zero;
         playerCurrentHealthSO.Int = playerMaxHealth;
+        shield.gameObject.SetActive(false);
 
         PCM.timer.timer.ModifyTimerMode(timerPos, TimerMode.Precise);
         PCM.timer.timer.SetTime(timerPos, secondLength);
@@ -48,6 +58,8 @@ public class PlayerSystems : MonoBehaviour
         PCM.timer.timer.SetAdditionalLoops( timerPos, -1);
         PCM.timer.timer.SubscribeToTimerIsZero(timerPos , OnHealthDrain);
         adjustHealthSO.onValueChanged += AdjustHealth;
+
+        PCM.timer.timer.SubscribeToTimerIsZero(iFrames, IframeOver);
     }
 
     // Update is called once per frame
@@ -62,6 +74,16 @@ public class PlayerSystems : MonoBehaviour
         CheckHealth();
     }
 
+    private void IframeOver(object sender, EventArgs e)
+    {
+        shield.transform.DOScale(Vector3.zero, shieldTweenDur)
+            .SetEase(Ease.InBack)
+            .onComplete += () => shield.gameObject.SetActive(true);
+    }
+    public void UseHealth(int amount)
+    {
+
+    }
     public void AdjustHealth(object sender, EventArgs e)
     {
         if(adjustHealthSO.Int < 0)
@@ -74,6 +96,9 @@ public class PlayerSystems : MonoBehaviour
             else
             {
                 PCM.timer.timer.SetTime(iFrames, iframeDur);
+                shield.gameObject.SetActive(true);
+                shield.transform.DOScale(scaleSize,shieldTweenDur)
+                    .SetEase(Ease.OutBack);
             }
         }
 
